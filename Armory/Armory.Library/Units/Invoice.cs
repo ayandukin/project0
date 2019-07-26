a7b86d0d8d24173219542c22b1146165e1123907
Alex <alexyandukin@gmail.com>
2019-07-25 23:11:28 -0500

    upd

---
+++ Armory/Armory.Library/Units/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Armory.Library.Units
+{
+    class Invoice
+    {
+        private static int invoiceID = 1;
+        private int orderID, quantity;
+        private string item;
+
+        public void MakeInvoice(int orderID, string name, int quantity)
+        {
+
+        }
+
+    }
+}
